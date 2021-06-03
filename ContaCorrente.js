830771cd02e7d1b838ce7f2f17a665f1ed479a40
Gabriel de Oliveira <77301257+bagriel22@users.noreply.github.com>
2021-06-03 20:10:33 -0300

    Add files via upload

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,15 +1,33 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
-    cliente;
-   
-  
-    transferir (valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-    
+    _cliente;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
     }
 
+    get cliente(){
+        return this._cliente;
+    }
+
+
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
+    
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(agencia ,cliente){
+        this.cliente = cliente;
+        this.agencia = agencia;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -19,12 +37,17 @@ export class ContaCorrente{
     }
 
     depositar(valor){
-        if(valor <= 0) {
+        if(valor <= 0)
+        {
             return;
-        
-        }
-        this._saldo += valor;
+        } 
+        this._saldo += valor;           
     }
 
+    tranferir(valor, conta){
         
-}  
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        
+    }
+}
